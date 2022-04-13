65bec54e031d4f86b022f6acef6819da2fb5db0c
miguelcortes0999 <62513516+miguelcortes0999@users.noreply.github.com>
2022-04-12 23:35:07 -0500

    Add files via upload

---
+++ src/OptimizadorLineal.py
@@ -0,0 +1,122 @@
+from math import round
+
+class Perfiles:
+    def __init__(self, cantidades, medidas, tipos=False):
+        #verificación de argumento de tipos
+        if tipos==False:
+            self.tipos_i=['Ref' for n in cantidades]
+        else:
+            #verificacion de tamaño de listas
+            if len(cantidades)==len(tipos):
+                self.tipos_i=tipos
+            else:
+                print('\tERROR longitud de listas de distintos tamaños')
+        #verificacion de tamaño de listas
+        if len(cantidades)==len(medidas):
+            self.cantidades_i=cantidades
+            self.medidas_i=medidas
+            #creacion de inventario
+            self.inventario=[]
+            for n,cantidad in enumerate(self.cantidades_i):
+                for m in range(cantidad):
+                    self.inventario.append([self.medidas_i[n],self.tipos_i[n]])
+        else:
+            print('\tERROR longitud de listas de distintos tamaños')
+
+    def Requerimientos(self, cantidades, medidas, tipos=False):
+        #verificación de argumento de tipos
+        if tipos==False:
+            self.tipos_r=['Ref' for n in cantidades]
+        else:
+            #verificacion de tamañao de listas
+            if len(cantidades)==len(tipos):
+                self.tipos_r=tipos
+        #verificacion de tamaño de listas
+        if len(cantidades)==len(medidas):        
+            self.cantidades_r=cantidades
+            self.medidas_r=medidas
+            #creacion de requerimientos
+            self.requerimiento=[]
+            for n,cantidad in enumerate(self.cantidades_r):
+                for m in range(cantidad):
+                    self.requerimiento.append([self.medidas_r[n],self.tipos_r[n]])
+        else:
+            print('\tERROR longitud de listas de distintos tamaños')
+    
+    def Optimizar_2D(self, ancho_corte=0):
+        #Organizar de amyor a menor para aplicacion de algortimo de optimizacion
+        self.inventario=sorted(self.inventario, key=lambda x: x[0],reverse=True)
+        self.requerimiento=sorted(self.requerimiento, key=lambda x: x[0],reverse=True)
+        self.faltantes=[]
+        self.asignacion=[]
+        #Aplicar algortimo por cada tipo_r
+        for tipo_r in set(self.tipos_r):
+            #Separar listas correspondientes al tipo
+            inventarios=list(filter(lambda x : x[1]==tipo_r , self.inventario))
+            requerimientos=list(filter(lambda x : x[1]==tipo_r , self.requerimiento))
+            #crear valor de residuo por perfil de inventario
+            for n,perfil_i in enumerate(inventarios):
+                inventarios[n].insert(0, perfil_i[0])
+            perfil_f=[]
+            #asignar todos los perfiles de requerimeinto del tipo
+            for n,perfil_r in enumerate(requerimientos):
+                for m,perfil_i in enumerate(inventarios):
+                    #si es poisble asigne el requerimeinto al perfil de inventario
+                    if perfil_i[1]>=perfil_r[0]:
+                        #se usa round para solucionar erro de binarios de lenguaje
+                        inventarios[m][1]=round((inventarios[m][1]-perfil_r[0]-ancho_corte)*10)/10
+                        inventarios[m].append(perfil_r[0])
+                        break
+                    #en caso de no poderse agregar y sea el ultimo perfil de inventario asignarlo a faltantes
+                    if m==int(len(inventarios))-1:
+                        print('Nunca se puedo agregar ',perfil_r[0],' de ',tipo_r)
+                        perfil_f.append(perfil_r[0])
+            #ceracion de faltantes
+            self.faltantes.append([tipo_r]+perfil_f)
+            #ceracion de asignacion
+            self.asignacion+=inventarios
+        return self.asignacion
+
+#funcion par aimprimir escalonado
+def print_straight(array):
+    for row in array:
+        print(row)  
+    print('\n')
+
+'''
+#Base 1
+#Objeto.Perfiles
+cantidades_i=[5,2,1]
+medidas_i=[250.0,600.0,150.0]
+#variable opcional en caso de multiples tipos
+#3er argumento opcional
+tipos_i=['Ref 744','Ref 8025','Ref 744']
+
+#Objeto.Requerimeintos
+cantidades_r=[7,12,2]
+medidas_r=[78.5,105.0,20.0]
+#variable obligatoria si incluyo tipos en la creaccio de Objeto
+tipos_r=['Ref 744','Ref 8025','Ref 744']
+
+#Objeto.Optimizar_2D
+#variable opcional en caso de ancho de corte>0, default=0
+ancho_corte=0.4
+
+#creacion de modelo de optimizacion corte lineal con grosor
+Objeto=Perfiles(cantidades_i,medidas_i,tipos_i)
+Objeto.Requerimientos(cantidades_r,medidas_r,tipos_r)
+Objeto.Optimizar_2D(ancho_corte)
+print_straight(Objeto.inventario)
+print_straight(Objeto.asignacion)
+print_straight(Objeto.faltantes)
+'''
+
+'''
+CONSULTA DE VARIABLES
+Objeto.tipos_i: tipos de perfiles de invntario
+Objeto.invantario: listas de inventario por tipo
+Objeto.tipos_r: tipos de perfiles de requerimiento
+Objeto.requerimiento: listas de requerimiento por tipo
+Objeto.asignacion: listas de asignacion por perfil (en posicion 0) con sobrante en 1ra posicion
+Objeto.faltantes: listas de perfiles faltantes por tipo
+'''
